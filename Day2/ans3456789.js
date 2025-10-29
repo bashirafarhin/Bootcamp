62b0222677916a98f8d97f6b3bb131c9ee124899
bashirafarhin <farhinbashira@gmail.com>
2025-10-29 21:19:39 +0530

    uploaded day-2 assignment

---
+++ Day2/ans3456789.js
@@ -0,0 +1,103 @@
+var arr = [1, 2, 3, 4, 5];
+
+
+
+// Add a number to the end of the array.
+arr.push(1);
+console.log(arr);
+
+
+
+// Remove the first number from the array.
+var firstElement = arr.shift();
+console.log(firstElement);
+
+
+
+// Insert a number at a specific index.
+arr.splice(1, 0, 100); // insert 2 at index 1
+console.log(arr[1]);
+
+
+
+// Replace all occurrences of a number with another number.
+arr = arr.map(function (ele) {
+  return ele === 1 ? 2 : ele;
+});
+console.log(arr);
+
+
+
+// Sort the array in ascending order(without using sort())
+function mergeSort(arr, start, end) {
+  if (start >= end) {
+    return;
+  }
+  var mid = Math.floor((end - start) / 2 + start);
+  mergeSort(arr, start, mid);
+  mergeSort(arr, mid + 1, end);
+  merge(arr, start, mid, end);
+  return;
+}
+
+
+
+function merge(arr, start, mid, end) {
+  var temp = [];
+  var left = start;
+  var right = mid + 1;
+  while (left <= mid && right <= end) {
+    if (arr[left] < arr[right]) {
+      temp.push(arr[left]);
+      left++;
+    } else {
+      temp.push(arr[right]);
+      right++;
+    }
+  }
+
+  while (left <= mid) {
+    temp.push(arr[left]);
+    left++;
+  }
+
+  while (right <= end) {
+    temp.push(arr[right]);
+    right++;
+  }
+
+  for (var k = 0; k < temp.length; k++) {
+    arr[start + k] = temp[k];
+  }
+}
+console.log(arr);
+
+
+
+// Reverse the array.(without using reverse())
+function reverse(arr) {
+  var start = 0;
+  var end = arr.length - 1;
+  while (start < end) {
+    var temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+  return arr;
+}
+console.log(arr);
+
+
+
+// Find the index of a specific number.
+function findIndex(arr, target) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+findIndex(arr, 1);
